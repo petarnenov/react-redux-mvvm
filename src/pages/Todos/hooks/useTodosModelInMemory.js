46eddc75fde2753e462f79c107ef2787a6832828
Petar Nenov <petarnenovpetrov@gmail.com>
2024-12-18 23:56:14 +0200

    update: inMemory initial data

--- src/pages/Todos/hooks/useTodosModelInMemory.js
+++ src/pages/Todos/hooks/useTodosModelInMemory.js
@@ -1,33 +1,32 @@
 import { useCallback, useState } from "react";
 
-const data = {
-    "todos": [
-        {
-            "id": "3",
-            "title": "Pay bills",
-            "description": "Electricity, water, and internet",
-            "status": false,
-            "dueDate": "2021-12-31"
-        },
-        {
-            "id": "4",
-            "title": "Go to the gym",
-            "description": "Cardio and strength training",
-            "status": false,
-            "dueDate": "2023-06-15"
-        },
-        {
-            "id": "5",
-            "title": "Call mom",
-            "description": "Check in and catch up",
-            "status": true,
-            "dueDate": "2023-06-10"
-        },
-    ]
-}
+const dataDI = [
+    {
+        "id": "3",
+        "title": "Pay bills",
+        "description": "Electricity, water, and internet",
+        "status": false,
+        "dueDate": "2021-12-31"
+    },
+    {
+        "id": "4",
+        "title": "Go to the gym",
+        "description": "Cardio and strength training",
+        "status": false,
+        "dueDate": "2023-06-15"
+    },
+    {
+        "id": "5",
+        "title": "Call mom",
+        "description": "Check in and catch up",
+        "status": true,
+        "dueDate": "2023-06-10"
+    },
+]
 
-const useTodosModel = () => {
-    const [todos, setTodos] = useState(()=>data.todos);
+
+const useTodosModel = (data = dataDI) => {
+    const [todos, setTodos] = useState(data);
 
     const deleteTodo = useCallback((id) => {
         setTodos(prevTodos => prevTodos.filter((todo) => todo.id !== id));
@@ -36,7 +35,7 @@ const useTodosModel = () => {
     return {
         todos,
         error: null,
-        isLoading: true,
+        isLoading: false,
         deleteTodo
     };
 };
